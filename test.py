c0fc555a7e0a0ea512f96e4ca15e1523bbd4116a
Wyatt Bunstine <wyattbunstine@columbus.rr.com>
2023-08-28 16:57:02 -0400

    Added cif files for SC dataset

--- test.py
+++ test.py
@@ -7,14 +7,26 @@ from pymatgen.symmetry.kpath import KPathSeek
 import numpy as np
 import pyxtal
 import pymatgen
+import pandas as pd
 
 # specify the path of an experimental structure
-struc_file = "OrbitalOverlap/Cu4BiSe4I.cif"
+df = pd.read_csv("database/all_data.csv")
+API_KEY = "<MP_API_KEY>"
+# inputs
+with MPRester(API_KEY) as mpr:
+    MPIDS = []
+    for index, row in df.iterrows():
+        if row['materials_project_id'] not in MPIDS:
+            MPIDS.append(row['materials_project_id'])
+        # save the relevent data to a dict
+        if len(MPIDS) == 10:
+            data = mpr.summary.search(material_ids=MPIDS)
+            for mat in data:
+                mat.structure.to("database/data/"+str(mat.material_id)+".cif")
+            MPIDS = []
+    data = mpr.summary.search(material_ids=MPIDS)
+    for mat in data:
+        mat.structure.to("database/data/" + str(mat.material_id) + ".cif")
+    MPIDS = []
 
-xtal1 = pyxtal.pyxtal()
-xtal1.from_seed(seed=struc_file, style='pyxtal')
-#print(xtal1)
-
-# visualize the structure
-xtal1.show(supercell=(2,2,1))
 
