239963c5acc268cca81932a1a99fe9d0f52a529f
Bunstine, Wyatt L <bunstine.6@buckeyemail.osu.edu>
2023-10-11 11:25:28 -0400

    Added config support for main and expanded transformer code

--- test.py
+++ test.py
@@ -1,20 +1,18 @@
 import json
-from mp_api.client import MPRester
+#from mp_api.client import MPRester
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 import pymatgen
 from pymatgen import core
 from pymatgen.symmetry.kpath import KPathSeek
 import numpy as np
-import pyxtal
 import pymatgen
+import pymatgen.analysis.diffraction.xrd
 
-# specify the path of an experimental structure
-struc_file = "OrbitalOverlap/Cu4BiSe4I.cif"
+struct = []
+struct.append(pymatgen.core.structure.Structure.from_file("YbMgGaO4_VP.cif"))
+struct.append(pymatgen.core.structure.Structure.from_file("YbMgGaO4_VPMg.cif"))
+struct.append(pymatgen.core.structure.Structure.from_file("YbMgGaO4_VPGa.cif"))
 
-xtal1 = pyxtal.pyxtal()
-xtal1.from_seed(seed=struc_file, style='pyxtal')
-#print(xtal1)
-
-# visualize the structure
-xtal1.show(supercell=(2,2,1))
-
+xrd = pymatgen.analysis.diffraction.xrd.XRDCalculator()
+fig = xrd.plot_structures(struct,annotate_peaks = None,ax_annotate=False)
+fig.show()
